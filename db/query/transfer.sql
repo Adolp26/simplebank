d92471cbe3b62b389d96ca791954718a27942498
unknown <adolpheribeiro7@gmail.com>
2024-04-12 01:07:11 -0300

    fix: refactor sql instructions

--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -1,12 +1,7 @@
- -- name: CreateTransfer :one
-INSERT INTO transfers (
-  from_account_id,
-  to_account_id,
-  amount
-) VALUES (
-  $1, $2, $3
-)
-RETURNING *;
+-- name: CreateTransfer :one
+INSERT INTO transfers (from_account_id, to_account_id, amount)
+VALUES ($1, $2, $3)
+RETURNING id, from_account_id, to_account_id, amount, created_at;
 
 -- name: GetTransfer :one
 SELECT * FROM transfers
